c28e9da54b712364f9780808a0913b0436c07967
pvictor <perrier.victor@gmail.com>
2019-02-25 16:14:44 +0100

    intro module welcome

--- modules/welcome-module.R
+++ modules/welcome-module.R
@@ -6,8 +6,17 @@
 welcome_UI <- function(id) {
   ns <- NS(id)
   modalDialog(
-    title = "Welcome to memory Hex game !",
-    "Blabla", 
+    title = tags$h1(
+      style = "text-align: center;",
+      "Welcome to memory Hex game !"
+    ),
+    tags$div(
+      style = "text-align: center;",
+      tags$p("Find all matching Hex as soon as possible!"),
+      tags$p("Click on an hex to return it"),
+      tags$p("You can see only two hex at the same time"),
+      tags$p("When you're ready, click button below to play !")
+    ), 
     footer = actionButton(
       inputId = ns("play"),
       label = "Play !",
